01d5ceb6943a185bea16b6360ec660969ccb6f04
Victor Nunes <victor.nunes@seriana.com.br>
2026-07-30 10:23:01 -0300

    adicionado preco de custo e indice de venda - prototipo - tipo wobller

---
+++ src/backend/CalculadoraCSharp/CalculadoraOrcamento/IndiceVenda.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculadoraOrcamento
+{
+    public class IndiceVenda
+    {
+        public static double CalcularIndiceVenda(int x)
+        {
+            return x;
+        }
+    }
+    }
+
--- src/backend/CalculadoraCSharp/CalculadoraOrcamento/Program.cs
+++ src/backend/CalculadoraCSharp/CalculadoraOrcamento/Program.cs
@@ -7,10 +7,47 @@
             Console.WriteLine("Sistema de Calculo de Orçamento - Seriana.com");
             Console.WriteLine($"Digite o tipo do produto; \n 1 - Wobller \n 2 - Testeira \n 3 - Outros \n 0 - Sair");
             int opcao = int.Parse(Console.ReadLine());
+            string servico, cliente;
+            int os;
+            int quantidade;
+            double materiaPrimaPreco, producaoPreco, terceirosPreco;
+            double margemLucro;
+            var mu = 0.20;
+            var ca = 0.05;
+            var cv = 0.03;
+            var ci = 0.02;
+            var ce = 0.03;
+            var nf = 0.15;
+            var tj = 0.03;
+            var somaporcentagem = mu + ca + cv + ci + ce + nf + tj;
+            double impostos;
+            double juros;
             switch (opcao)
             {
                 case 1:
-                    Console.WriteLine("Wobller selecionado.");
+                    Console.WriteLine("Nome do Cliente:");
+                    cliente = Console.ReadLine();
+                    Console.WriteLine("Serviço:");
+                    servico = Console.ReadLine();
+                    Console.WriteLine("Número da OS:");
+                    os = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Quantidade:");
+                    quantidade = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Materia Prima - Preço:");
+                    materiaPrimaPreco = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Produção - Preço:");
+                    producaoPreco = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Terceiros - Preço:");
+                    terceirosPreco = double.Parse(Console.ReadLine());
+
+                    Console.Clear();
+                    Console.Write("------------------------------------------------------------------------------\n");
+                    double PrecoCusto = materiaPrimaPreco + producaoPreco + terceirosPreco;
+                    Console.WriteLine($"Preço de Custo: {PrecoCusto}");
+                    Console.Write("------------------------------------------------------------------------------\n");
+                    var indice = 1 - somaporcentagem;
+                    Console.WriteLine($"Indice de Venda: {indice} ");
+
                     break;
                 case 2:
                     Console.WriteLine("Testeira selecionada.");
@@ -25,9 +62,11 @@
                 default:
                     Console.WriteLine("Opção inválida.");
                     break;
-
              
             }
+           
+
+
             Console.ReadLine();
         }
     }
